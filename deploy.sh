5dae83b038c2588bdc6bd614cba1964ac16e7892
Robin Long <binlon@amazon.com>
2022-03-03 17:39:51 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,11 @@ then
     docker build ./service -t $IMAGE_NAME:latest
     last_image_id=$(docker images | awk '{print $3}' | awk 'NR==2')
     account=$(aws sts get-caller-identity --query Account --output text)
-    docker tag $last_image_id $account.dkr.ecr.eu-west-1.amazonaws.com/$REPO_NAME
-    echo docker push $account.dkr.ecr.eu-west-1.amazonaws.com/$REPO_NAME
+    region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+    docker tag $last_image_id $account.dkr.ecr.$region.amazonaws.com/$REPO_NAME
+    echo docker push $account.dkr.ecr.$region.amazonaws.com/$REPO_NAME
     aws ecr describe-repositories --repository-names $REPO_NAME || aws ecr create-repository --repository-name $REPO_NAME
-    docker push $account.dkr.ecr.eu-west-1.amazonaws.com/$REPO_NAME
+    docker push $account.dkr.ecr.$region.amazonaws.com/$REPO_NAME
 else
   echo Skipping build
 fi
